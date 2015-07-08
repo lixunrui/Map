464df0effd6381ec62f58ad386ee742dbabe0736
Raymond <raymond>
2015-07-08 16:28:43 +1200

    - init checked in

---
+++ Map App/Map App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map App
+//
+//  Created by Raymond on 8/07/15.
+//  Copyright (c) 2015 Raymond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Map App/Map AppTests/Map_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Map_AppTests.swift
+//  Map AppTests
+//
+//  Created by Raymond on 8/07/15.
+//  Copyright (c) 2015 Raymond. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Map_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
